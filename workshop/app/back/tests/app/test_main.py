47a92a1f7a57f54893bc08df720a0ea9fe045f43
mjcc30 <maxime.cordeiro@gmail.com>
2025-11-29 15:30:55 +0100

    fix: add version in python test

--- workshop/app/back/tests/app/test_main.py
+++ workshop/app/back/tests/app/test_main.py
@@ -7,4 +7,4 @@ client = TestClient(app)
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Welcome to the Star Wars API"}
+    assert response.json() == {"message": "Welcome to the Star Wars API v1"}
